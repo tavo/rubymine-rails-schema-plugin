b6ae0cb573233091241b48b0614a979e981cdaee
GitHub Action <action@github.com>
2023-12-01 04:16:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/tavo/rubyminerailsschemaplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/tavo/rubyminerailsschemaplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.tavo.rubyminerailsschemaplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.tavo.rubyminerailsschemaplugin.MyBundle
+import com.github.tavo.rubyminerailsschemaplugin.services.MyProjectService
 import javax.swing.JButton
 
 
